d1dbe70fcc27cfda008c1d36f564977489282906
Dipesh Bajgain <dipbazz@gmail.com>
2022-01-15 15:28:22 +0545

    Add videos listview in the detail section

--- lib/video_info.dart
+++ lib/video_info.dart
@@ -13,10 +13,12 @@ class VideoInfo extends StatefulWidget {
 }
 
 class _VideoInfoState extends State<VideoInfo> {
-    List exercises = [];
+  List exercises = [];
 
   _initData() {
-    DefaultAssetBundle.of(context).loadString("json/videoinfo.json").then((value) {
+    DefaultAssetBundle.of(context)
+        .loadString("json/videoinfo.json")
+        .then((value) {
       exercises = json.decode(value);
     });
   }
@@ -93,31 +95,28 @@ class _VideoInfoState extends State<VideoInfo> {
                         width: 100,
                         height: 30,
                         decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          gradient: LinearGradient(
-                            colors: [
-                              color.AppColor.secondPageContainerGradient1stColor,
-                              color.AppColor.secondPageContainerGradient2ndColor
-                            ],
-                            begin: Alignment.bottomLeft,
-                            end: Alignment.topRight
-                          )
-                        ),
+                            borderRadius: BorderRadius.circular(10),
+                            gradient: LinearGradient(
+                                colors: [
+                                  color.AppColor
+                                      .secondPageContainerGradient1stColor,
+                                  color.AppColor
+                                      .secondPageContainerGradient2ndColor
+                                ],
+                                begin: Alignment.bottomLeft,
+                                end: Alignment.topRight)),
                         child: Row(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
-                            Icon(
-                              Icons.timer,
-                              size: 20,
-                              color: color.AppColor.secondPageIconColor
-                            ),
+                            Icon(Icons.timer,
+                                size: 20,
+                                color: color.AppColor.secondPageIconColor),
                             const SizedBox(width: 5),
                             Text(
                               "60 min",
                               style: TextStyle(
-                                fontSize: 16,
-                                color: color.AppColor.secondPageIconColor
-                              ),
+                                  fontSize: 16,
+                                  color: color.AppColor.secondPageIconColor),
                             )
                           ],
                         ),
@@ -127,31 +126,28 @@ class _VideoInfoState extends State<VideoInfo> {
                         width: 230,
                         height: 30,
                         decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          gradient: LinearGradient(
-                            colors: [
-                              color.AppColor.secondPageContainerGradient1stColor,
-                              color.AppColor.secondPageContainerGradient2ndColor
-                            ],
-                            begin: Alignment.bottomLeft,
-                            end: Alignment.topRight
-                          )
-                        ),
+                            borderRadius: BorderRadius.circular(10),
+                            gradient: LinearGradient(
+                                colors: [
+                                  color.AppColor
+                                      .secondPageContainerGradient1stColor,
+                                  color.AppColor
+                                      .secondPageContainerGradient2ndColor
+                                ],
+                                begin: Alignment.bottomLeft,
+                                end: Alignment.topRight)),
                         child: Row(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
-                            Icon(
-                              Icons.handyman_outlined,
-                              size: 20,
-                              color: color.AppColor.secondPageIconColor
-                            ),
+                            Icon(Icons.handyman_outlined,
+                                size: 20,
+                                color: color.AppColor.secondPageIconColor),
                             const SizedBox(width: 5),
                             Text(
                               "Resistent band, kettlebell",
                               style: TextStyle(
-                                fontSize: 16,
-                                color: color.AppColor.secondPageIconColor
-                              ),
+                                  fontSize: 16,
+                                  color: color.AppColor.secondPageIconColor),
                             )
                           ],
                         ),
@@ -165,46 +161,92 @@ class _VideoInfoState extends State<VideoInfo> {
               child: Container(
                 padding: const EdgeInsets.only(top: 25, left: 25, right: 25),
                 decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(topRight: Radius.circular(70))
-                ),
+                    color: Colors.white,
+                    borderRadius:
+                        BorderRadius.only(topRight: Radius.circular(70))),
                 child: Column(
                   children: [
-                    Row(
-                      children: [
-                        Text(
-                          "Circuit 1: Legs Toning",
-                          style: TextStyle(
+                    Row(children: [
+                      Text(
+                        "Circuit 1: Legs Toning",
+                        style: TextStyle(
                             fontSize: 18,
                             fontWeight: FontWeight.bold,
-                            color: color.AppColor.circuitsColor
-                          ),
-                        ),
-                        Expanded(child: Container()),
-                        Row(
-                          children: [
-                            Icon(
-                              Icons.loop,
-                              size: 20,
-                              color: color.AppColor.loopColor
+                            color: color.AppColor.circuitsColor),
+                      ),
+                      Expanded(child: Container()),
+                      Row(
+                        children: [
+                          Icon(Icons.loop,
+                              size: 20, color: color.AppColor.loopColor),
+                          const SizedBox(width: 10),
+                          Text(
+                            "3 sets",
+                            style: TextStyle(
+                              fontSize: 15,
+                              color: color.AppColor.setsColor,
                             ),
-                            const SizedBox(width: 10),
-                            Text(
-                              "3 sets",
-                              style: TextStyle(
-                                fontSize: 15,
-                                color: color.AppColor.setsColor,
-                              ),
-                            )
-                          ],
-                        )
-                      ]
-                    )
+                          )
+                        ],
+                      ),
+                    ]),
+                  Expanded(
+                    child: OverflowBox(
+                      maxWidth: MediaQuery.of(context).size.width,
+                      child: ListView.builder(
+                        padding: const EdgeInsets.all(25),
+                        itemCount: 4,
+                        itemBuilder: (_, i) {
+                          return Container(
+                            margin: const EdgeInsets.only(bottom: 15),
+                            child: Column(
+                              children: [
+                                Row(
+                                  children: [
+                                    Container(
+                                      height: 80,
+                                      width: 100,
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(10),
+                                        image: const DecorationImage(
+                                          image: AssetImage("assets/squat1.jpg"),
+                                          fit: BoxFit.fill
+                                        )
+                                      )
+                                    ),
+                                    const SizedBox(width: 15),
+                                    Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Text("You are doing great.",
+                                            style: TextStyle(
+                                              fontSize: 18,
+                                              fontWeight: FontWeight.bold,
+                                              color: color.AppColor.circuitsColor,
+                                            )),
+                                        const SizedBox(height: 10),
+                                        RichText(
+                                          text: TextSpan(
+                                            text: "Keep it up",
+                                            style: TextStyle(
+                                              color: color.AppColor.homePagePlanColor,
+                                              fontSize: 16,
+                                            ),
+                                          )
+                                        ),
+                                      ],
+                                    )
+                                  ],
+                                ),
+                              ],
+                            ),
+                          );
+                        }))
+                    ),
                   ],
                 ),
               ),
             ),
-
           ],
         ),
       ),
